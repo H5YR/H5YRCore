1ad3337b2a2f1ad019c9f025d8fa917ab390528a
Owain Williams <owaingdwilliams@gmail.com>
2019-05-05 19:42:31 +0100

    A copy of the H5YR core code

---
+++ Controllers/TweetController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Tweetinvi;
+using Umbraco.Web.Mvc;
+using H5YR.Core.Models;
+
+using System.Linq;
+using System.Web.Configuration;
+
+namespace H5YR.Core.Controllers
+{
+    public class TweetController : SurfaceController
+    {
+
+
+
+        public ActionResult Index()
+        {
+
+            var consumerKey = WebConfigurationManager.AppSettings["twitterConsumerKey"];
+            var consumerSecret = WebConfigurationManager.AppSettings["twitterConsumerSecret"];
+            var accessToken = WebConfigurationManager.AppSettings["twitterAccessToken"];
+            var accessTokenSecret = WebConfigurationManager.AppSettings["twitterAccessTokenSecret"];
+
+
+            // You need to make sure your app on dev.twitter.com has read and write permissions if you wish to tweet!
+            var creds = Auth.SetUserCredentials(consumerKey, consumerSecret, accessToken, accessTokenSecret);
+            Tweetinvi.User.GetAuthenticatedUser(creds);
+
+            var searchResults = Search.SearchTweets("#h5yr");
+ 
+
+            List<TweetModel> FetchTweets = new List<TweetModel>();
+
+
+            foreach (var tweet in searchResults)
+            {
+                FetchTweets.Add(new TweetModel()
+                {
+
+                    Username = tweet.CreatedBy.ToString(),
+                    Avatar = tweet.CreatedBy.ProfileImageUrl.ToString(),
+                    Twit = tweet.Text,
+                    ScreenName = tweet.CreatedBy.ScreenName.ToString(),
+                    TweetedOn = tweet.CreatedAt,
+                    NumberOfTweets = FetchTweets.Count(),
+                    ReplyToTweet = tweet.IdStr
+
+            });
+
+               
+            };
+
+            return PartialView("~/Views/Tweets/Index.cshtml", FetchTweets);
+        }
+
+
+    }
+
+
+}
---
+++ Models/TweetModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H5YR.Core.Models
+{
+
+    public class TweetModel
+    {
+        
+        public string Twit { get; set; }
+        public string Username { get; set; }
+        public string ScreenName { get; set; }
+        public string Avatar { get; set; }
+        public DateTime TweetedOn { get; set; }
+        public int NumberOfTweets { get; set; }  
+        public string ReplyToTweet { get; set; }
+
+
+    }
+
+}
